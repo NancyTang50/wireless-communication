fe31fb80a2e21076a18410ff0349a06afb662780
Bram <bram.esendam@gmail.com>
2023-11-29 15:35:15 +0100

    Added basis of the clean architecture

--- src/app/WirelessCom.UI/MauiProgram.cs
+++ src/app/WirelessCom.UI/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using WirelessCom.UI.Extensions;
 
 namespace WirelessCom.UI;
 
@@ -12,6 +13,7 @@ public static class MauiProgram
             .ConfigureFonts(fonts => { fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular"); });
 
         builder.Services.AddMauiBlazorWebView();
+        builder.Services.RegisterDependencies();
 
         #if DEBUG
         builder.Services.AddBlazorWebViewDeveloperTools();
