0604d13ccb25d002132f7289f16c60c2dd13a38c
Tim Kremer <494033@student.saxion.nl>
2023-11-29 15:24:35 +0100

    added packages and made cross compile work

--- src/pi/src/main.rs
+++ src/pi/src/main.rs
@@ -1,3 +1,8 @@
+use btleplug::api::bleuuid::uuid_from_u16;
+use uuid::Uuid;
+
+const TEMPERATURE_CHARACTERISTIC_UUID : Uuid = uuid_from_u16(0xFFE9);
+
 fn main() {
     println!("Hello, world!");
 }
