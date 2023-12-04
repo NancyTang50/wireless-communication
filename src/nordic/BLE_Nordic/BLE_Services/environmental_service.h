9b5ddc58744687a9500db77b7f124662b6642bbc
Nancy Tang <494032@student.saxion.nl>
2023-12-04 22:05:19 +0100

    Implemented the environmental service

--- src/nordic/BLE_Nordic/BLE_Services/environmental_service.h
+++ src/nordic/BLE_Nordic/BLE_Services/environmental_service.h
@@ -1,3 +1,50 @@
+#include "ble/BLE.h"
+#include <cstdint>
+#include <stdint.h>
+
+#define TEMPERATURE_CHARACTERISTIC      0x2A6E
+#define HUMIDITY_CHARACTERISTIC         0x2A6F
+
+class EnvironmentalService{
+    private:
+        BLE &ble;
+        int16_t temperature;
+        uint16_t humidity;
+
+        ReadOnlyGattCharacteristic<int16_t> temperatureCharacteristic;
+        ReadOnlyGattCharacteristic<uint16_t> humidityCharacteristic;
+
+    public:
+    EnvironmentalService(BLE &_ble, int16_t temperature = 0, uint16_t humidity = 0):
+        ble(_ble),
+        temperature(temperature),
+        humidity(humidity),
+        temperatureCharacteristic(TEMPERATURE_CHARACTERISTIC, &temperature, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY), 
+        humidityCharacteristic(HUMIDITY_CHARACTERISTIC, &humidity, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY)
+    {
+        GattCharacteristic *charTable[] = {&temperatureCharacteristic, &humidityCharacteristic};
+        GattService         environmentalService(GattService::UUID_ENVIRONMENTAL_SERVICE, charTable, sizeof(charTable) / sizeof(GattCharacteristic*));
+
+        ble.addService(environmentalService);
+    }
+
+    void updateTemperature(float newTemperature){
+        temperature = (int16_t)(newTemperature * 100);
+        ble.gattServer().write(temperatureCharacteristic.getValueHandle(), (uint8_t*)&temperature, sizeof(int16_t));
+    } 
+
+    void updateHumidity(int16_t newHumidity){
+        humidity = (int16_t)(newHumidity * 100);
+        ble.gattServer().write(humidityCharacteristic.getValueHandle(), (uint8_t*)&humidity, sizeof(int16_t));
+    }
+    
+};
+
+
+
+
+
+
 // #ifndef CLIMATE_SERVICE_H_
 // #define CLIMATE_SERVICE_H_
 
