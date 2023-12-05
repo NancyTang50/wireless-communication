1d8b5effa35eaa1bf909152076f4ddc3572d82cd
TKremer00 <38754668+TKremer00@users.noreply.github.com>
2023-12-05 14:38:07 +0100

    deleted deploy script

--- src/pi/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pi_ssh_ip="nancy@192.168.178.158"
-binary_file="./target/aarch64-unknown-linux-gnu/debug/pi"
-output_folder="/home/nancy/"
-
-scp "$binary_file" "$pi_ssh_ip:$output_folder"
-
-ssh $pi_ssh_ip
