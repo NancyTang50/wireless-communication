685af28312adf8a9663d59791bb47a9d6e917a7a
Nancy Tang <494032@student.saxion.nl>
2023-12-14 16:12:08 +0100

    Implement the temperature and humidity

--- src/platformio/Projects/ble_platformio/src/main.cpp
+++ src/platformio/Projects/ble_platformio/src/main.cpp
@@ -7,9 +7,16 @@
 #include <BLEPeripheral.h>
 #include <DHT.h>
 #include <Adafruit_Sensor.h>
+#include <arduino-timer.h>
 
-// LED pin
+// DHT22 sensor pin
 #define DHT22_PIN A5
+
+Timer<> timer = timer_create_default();
+
+volatile bool readFromSensor = false;
+
+// NRF52_ISR_Timer SensorReadTimer;
 
 // custom boards may override default pin definitions with BLEPeripheral(PIN_REQ, PIN_RDY, PIN_RST)
 BLEPeripheral blePeripheral = BLEPeripheral();
@@ -18,12 +25,15 @@ BLEPeripheral blePeripheral = BLEPeripheral();
 BLEService environmentalService = BLEService("0000181A00001000800000805F9B34FB");
 
 // The temperature characteristic
-BLECharCharacteristic temperatureCharacteristic = BLECharCharacteristic("00002A6E00001000800000805F9B34FB", BLERead | BLENotify);
+BLEUnsignedShortCharacteristic temperatureCharacteristic = BLEUnsignedShortCharacteristic("00002A6E00001000800000805F9B34FB", BLERead | BLENotify);
+// FIXME: Add descriptor, or should we remove those?
+
 // The humidity characteristic
-BLECharCharacteristic humidityCharacteristic = BLECharCharacteristic("00002A6F00001000800000805F9B34FB", BLERead | BLENotify);
+BLEUnsignedShortCharacteristic humidityCharacteristic = BLEUnsignedShortCharacteristic("00002A6F00001000800000805F9B34FB", BLERead | BLENotify);
+// FIXME: Add descriptor, or should we remove those?
 
 DHT dht(DHT22_PIN, DHT22); 
-float humidity, temperature;
+float lastHumidity, lastTemperature;
 
 // void loop() {
 //   humidity = dht.readHumidity();
@@ -39,62 +49,132 @@ float humidity, temperature;
 
 // }
 
+boolean significantChange(float val1, float val2, float threshold) {
+    return (abs(val1 - val2) >= threshold);
+}
+
+void blePeripheralConnectHandler(BLECentral& central) {
+    Serial.print(F("Connected event, central: "));
+    Serial.println(central.address());
+}
+
+void blePeripheralDisconnectHandler(BLECentral& central) {
+    Serial.print(F("Disconnected event, central: "));
+    Serial.println(central.address());
+}
+
+void timerHandler() {
+    // SensorReadTimer.run();
+}
+
+bool updateReadSensor(void *) {
+    readFromSensor = true;
+    return true; // NOTE: this is to repeat the timer
+}
+
+void setTempCharacteristicValue() {
+  float reading = dht.readTemperature();
+  if (!isnan(reading) && significantChange(lastTemperature, reading, 0.5)) {
+    Serial.print(F("Temperature: ")); Serial.print(reading); Serial.println(F("C"));
+    
+    temperatureCharacteristic.setValue(reading * 100);
+    lastTemperature = reading;
+  }
+}
+
+void setHumidityCharacteristicValue() {
+  float reading = dht.readHumidity();
+  if (!isnan(reading) && significantChange(lastHumidity, reading, 1.0)) {
+    Serial.print(F("Humidity: ")); Serial.print(reading); Serial.println(F("%"));
+
+    humidityCharacteristic.setValue(reading * 100);
+    lastHumidity = reading;
+  }
+}
+
 void setup()
 {
-  Serial.begin(9600);
-  dht.begin();
+    Serial.begin(9600);
 #if defined(__AVR_ATmega32U4__)
-  delay(5000); // 5 seconds delay for enabling to see the start up comments on the serial board
+    delay(5000); // 5 seconds delay for enabling to see the start up comments on the serial board
 #endif
 
-  // set LED pin to output mode
-  // pinMode(LED_PIN, OUTPUT);
+    dht.begin();
 
-  // set advertised local name and service UUID
-  blePeripheral.setLocalName("LED");
-  blePeripheral.setAdvertisedServiceUuid(environmentalService.uuid());
+    blePeripheral.setLocalName("SOME_NAME_NORDIC");
+    blePeripheral.setAdvertisedServiceUuid(environmentalService.uuid());
 
-  // add service and characteristic
-  blePeripheral.addAttribute(environmentalService);
-  blePeripheral.addAttribute(temperatureCharacteristic);
+    // add service and characteristic
+    blePeripheral.addAttribute(environmentalService);
+    blePeripheral.addAttribute(temperatureCharacteristic);
+    blePeripheral.addAttribute(humidityCharacteristic);
 
-  // begin initialization
-  blePeripheral.begin();
+    blePeripheral.setEventHandler(BLEConnected, blePeripheralConnectHandler);
+    blePeripheral.setEventHandler(BLEDisconnected, blePeripheralDisconnectHandler);
 
-  Serial.println(F("BLE LED Peripheral"));
+    // begin initialization
+    blePeripheral.begin();
+
+    timer.every(2000, updateReadSensor);
+
+    // Interval in microsecs
+    // if (ITimer.attachInterruptInterval(1000, timerHandler))
+    // {
+    //     Serial.print(F("Starting ITimer OK, millis() = "));
+    //     Serial.println(millis());
+    // }
+    // else {
+    //     Serial.println(F("Can't set ITimer. Select another freq. or timer"));
+    // }
+
+    // SensorReadTimer.setInterval(2000L, updateReadSensor);
+    Serial.println(F("BLE LED Peripheral"));
 }
 
 void loop()
 {
-  BLECentral central = blePeripheral.central();
+    blePeripheral.poll();
 
-  if (central)
-  {
-    // central connected to peripheral
-    Serial.print(F("Connected to central: "));
-    Serial.println(central.address());
-
-    while (central.connected())
-    {
-      // central still connected to peripheral
-      if (temperatureCharacteristic.written())
-      {
-        // central wrote new value to characteristic, update LED
-        if (temperatureCharacteristic.value())
-        {
-          Serial.println(F("LED on"));
-          // digitalWrite(LED_PIN, LOW);
-        }
-        else
-        {
-          Serial.println(F("LED off"));
-          // digitalWrite(LED_PIN, HIGH);
-        }
-      }
+    if(readFromSensor) { // NOTE: this is some volitile boolean, on a interup timer
+        setTempCharacteristicValue();
+        setHumidityCharacteristicValue();
+        readFromSensor = false;
     }
 
-    // central disconnected
-    Serial.print(F("Disconnected from central: "));
-    Serial.println(central.address());
-  }
+    timer.tick();
+  
+  // BLECentral central = blePeripheral.central();
+
+  // if (central)
+  // {
+  //   // central connected to peripheral
+  //   Serial.print(F("Connected to central: "));
+  //   Serial.println(central.address());
+
+  //   while (central.connected())
+  //   {
+  //     // central still connected to peripheral
+  //     if (temperatureCharacteristic.written())
+  //     {
+  //       // central wrote new value to characteristic, update LED
+  //       if (temperatureCharacteristic.value())
+  //       {
+  //         Serial.println(F("LED on"));
+  //         // digitalWrite(LED_PIN, LOW);
+  //       }
+  //       else
+  //       {
+  //         Serial.println(F("LED off"));
+  //         // digitalWrite(LED_PIN, HIGH);
+  //       }
+  //     }
+  //   }
+
+  //   // central disconnected
+  //   Serial.print(F("Disconnected from central: "));
+  //   Serial.println(central.address());
+  // }
 }
+
+
+
