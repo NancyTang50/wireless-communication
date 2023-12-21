4e95f5a8285595e6e8324943ffce72db921ccefa
Bram <bram.esendam@gmail.com>
2023-12-21 13:49:18 +0100

    Added BleDecoding

---
+++ src/app/WirelessCom.Application/Helpers/BleDecoding.cs
@@ -0,0 +1,14 @@
+﻿namespace WirelessCom.Application.Helpers;
+
+public static class BleDecoding
+{
+    public static decimal BleBytesToInt(byte[] bytes)
+    {
+        if (bytes.Length != 2)
+        {
+            throw new ArgumentException("Byte array must be of length 2", nameof(bytes));
+        }
+
+        return BitConverter.ToInt16(bytes, 0) / 100.0m;
+    }
+}
---
+++ src/app/tests/WirelessCom.Application.Tests/Helpers/BleDecodingTests.cs
@@ -0,0 +1,23 @@
+﻿using WirelessCom.Application.Helpers;
+
+namespace WirelessCom.Application.Tests.Helpers;
+
+[TestFixture]
+public class BleDecodingTests
+{
+    [Test]
+    public void BleDecoding_BleBytesToInt_ThrowsArgumentException_WhenByteArrayLengthIsNotTwo()
+    {
+        var bytes = new byte[] { 0x00, 0x00, 0x00 };
+        var exception = Assert.Throws<ArgumentException>(() => BleDecoding.BleBytesToInt(bytes));
+
+        Assert.That(exception!.Message, Is.EqualTo("Byte array must be of length 2 (Parameter 'bytes')"));
+    }
+    
+    [TestCase(new byte[] { 252, 8 }, 23)]
+    [TestCase(new byte[] { 170, 20 }, 52.9)]
+    public void BleDecoding_BleBytesToInt_ReturnsCorrectValue_WhenByteArrayLengthIsTwo(byte[] bytes, decimal expected)
+    {
+        Assert.That(BleDecoding.BleBytesToInt(bytes), Is.EqualTo(expected));
+    }
+}
