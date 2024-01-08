c8b75e4a667cd4d9db321c1c15dc45d4b9a7bd76
Bram <bram.esendam@gmail.com>
2024-01-08 20:58:07 +0100

    Added toast messages

---
+++ src/app/WirelessCom.Application/Services/IToastService.cs
@@ -0,0 +1,15 @@
+﻿namespace WirelessCom.Application.Services;
+
+public interface IToastService
+{
+    /// <summary>
+    ///     Show a toast message to the user.
+    /// </summary>
+    /// <param name="message">The message that will be shown in the toast.</param>
+    /// <param name="fontSize">The font size of the toast message. Default 14.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>
+    ///     A task that represents the asynchronous show operation.
+    /// </returns>
+    Task ShowToastAsync(string message, double fontSize = 14, CancellationToken cancellationToken = default);
+}
