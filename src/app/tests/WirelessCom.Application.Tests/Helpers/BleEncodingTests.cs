46c0cf066df983d1665caf40aa89f61c756a2f5d
Bram <bram.esendam@gmail.com>
2024-01-09 14:47:08 +0100

    Added BleEncoding & unit test

---
+++ src/app/tests/WirelessCom.Application.Tests/Helpers/BleEncodingTests.cs
@@ -0,0 +1,28 @@
+﻿using WirelessCom.Application.Helpers;
+
+namespace WirelessCom.Application.Tests.Helpers;
+
+[TestFixture]
+public class BleEncodingTests
+{
+    [Test]
+    public void BleEncoding_ShouldEncodeDateTimeBytes()
+    {
+        var expected = new byte[]
+        {
+            232, 7, 1, 6, 22, 5, 57, 6
+        };
+
+        var dateTime = new DateTime(
+            2024,
+            1,
+            6,
+            22,
+            5,
+            57
+        );
+
+        var result = BleEncoding.GetDateTimeBleBytes(dateTime);
+        result.Should().BeEquivalentTo(expected);
+    }
+}
